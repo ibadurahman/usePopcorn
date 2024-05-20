00cd956d46437c0af9591b0dff6dd15afd4bf358
Ibadurahman Hanif <ibadurahmanhanif@gmail.com>
2024-05-20 17:29:20 +0700

    update useeffect dependencies

--- src/App.js
+++ src/App.js
@@ -229,7 +229,7 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
           setIsError("");
           setIsLoading(true);
           const res = await fetch(
-            `http://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
+            `https://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
           );
           const data = await res.json();
           setMovie(data);
